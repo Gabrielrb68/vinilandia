e6a57960e6d08cc10a54c6d4d941bbb49ac91634
Bernardo <benckebernardo@gmail.com>
2022-11-04 14:32:39 -0300

    commit 4-10
    .

---
+++ src/modelo/Usuario.java
@@ -0,0 +1,15 @@
+package modelo;
+
+public class Usuario {
+	
+	public String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
