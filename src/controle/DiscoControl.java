1443162752edb33d96e7441e8383af68db266f29
Bernardo <benckebernardo@gmail.com>
2022-12-01 21:15:35 -0300

    Model Disco DiscoControl

---
+++ src/controle/DiscoControl.java
@@ -0,0 +1,11 @@
+package controle;
+
+import java.util.ArrayList;
+
+import modelo.Cliente;
+
+public class DiscoControl {
+	
+	private static ArrayList<Disco> tabelaDisco;
+
+}
---
+++ src/modelo/Disco.java
@@ -0,0 +1,49 @@
+package modelo;
+
+public class Disco {
+	
+	private String genero;
+	private String descrição;
+	private float preco;
+	private String comentar;
+	private float avaliacao;
+	//private ? foto;
+
+	private String autor;
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public String getDescrição() {
+		return descrição;
+	}
+	public void setDescrição(String descrição) {
+		this.descrição = descrição;
+	}
+	public float getPreco() {
+		return preco;
+	}
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	public String getComentar() {
+		return comentar;
+	}
+	public void setComentar(String comentar) {
+		this.comentar = comentar;
+	}
+	public float getAvaliacao() {
+		return avaliacao;
+	}
+	public void setAvaliacao(float avaliacao) {
+		this.avaliacao = avaliacao;
+	}
+}
